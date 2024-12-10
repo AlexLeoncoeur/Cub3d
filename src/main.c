4d9bbcb92d08c2979a388b2737307c3c29cc4adb
alexleoncoeur <alejandroarenasleon2@gmail.com>
2024-12-10 13:09:51 +0100

    feat: white screen

--- src/main.c
+++ src/main.c
@@ -6,13 +6,37 @@
 /*   By: aarenas- <aarenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 15:56:25 by aarenas-          #+#    #+#             */
-/*   Updated: 2024/12/09 15:59:05 by aarenas-         ###   ########.fr       */
+/*   Updated: 2024/12/10 13:05:35 by aarenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
+void	ft_resize(int width, int height, void *param)
+{
+	(void)param;
+	(void)width;
+	(void)height;
+	mlx_set_setting(MLX_STRETCH_IMAGE, 1);
+}
+
 int	main(void)
 {
-	printf("pipo\n");
+	mlx_t			*id;
+	mlx_image_t		*img;
+
+	id = mlx_init(1920, 1080, "cub3d", true);
+	if (!id)
+		exit(EXIT_FAILURE);
+	img = mlx_new_image(id, 1920, 1080);
+	ft_memset(img->pixels, 255, img->width * img->height * sizeof(int32_t));
+	mlx_image_to_window(id, img, 0, 0);
+	mlx_resize_hook(id, ft_resize, NULL);
+	mlx_loop(id);
+	mlx_terminate(id);
+	return (0);
 }
+	//t_game_core		*data; //estructura con datos del mapa y juego
+
+	/* if (!id)
+		ft_error_exit("Error: Could not initialize identifier\n"); */
--- src/ray_casting/ray_casting.c
+++ src/ray_casting/ray_casting.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   ray_casting.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aarenas- <aarenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/05 12:35:55 by aarenas-          #+#    #+#             */
-/*   Updated: 2024/12/10 12:50:57 by aarenas-         ###   ########.fr       */
+/*   Created: 2024/12/09 16:52:39 by aarenas-          #+#    #+#             */
+/*   Updated: 2024/12/09 16:53:09 by aarenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include "../include/cub3d.h"
 
-# include "MLX42/include/MLX42/MLX42.h"
-# include "libft/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-
-/* -- main.c -- */
-void	ft_resize(int width, int height, void *param);
-
-#endif
